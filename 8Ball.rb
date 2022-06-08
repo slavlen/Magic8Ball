56fb469345ec0356d3406b0ca3ba4c91d12cc374
Slavlen <slavlen2001@gmail.com>
2022-06-08 17:58:30 +0300

    first commit

---
+++ 8Ball.rb
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+answers = [
+    #  affirmative answers
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+  
+    # non-committal answers
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+  
+    # negative answers
+    'Don\'t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful'
+  ]
+
+  puts answers.sample
